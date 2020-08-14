2513a42ca4be14d00388794a161a8b2778c6bad7
mohmedmahmoud <bouhmed1996@gmail.com>
2020-08-15 00:36:08 +0200

    apllicat sur loop

--- src/app/App.java
+++ src/app/App.java
@@ -1,16 +1,30 @@
 package app;
+
 import java.util.Scanner;
 
-
 public class App {
-    public static void main(String[] args) throws Exception {
-      double moyen;
-      Scanner input =new Scanner(System.in);
-      System.out.println("quel est votre moyen general");
-      moyen=input.nextDouble();
-      if(moyen>= 10.0)
-      System.out.println("valide");
-      else 
-      System.out.println("no valide");
+    public static void main(final String[] args) throws Exception {
+        final int x = 9;
+        // for(int i=1;i < x;i++){
+        // System.out.println("nombre "+i);
+        // }
+        int i = 0;
+        // while (i < x) {
+        // System.out.println("nombre " + i);
+        // i++;
+        // }
+        // do {
+        //     System.out.println("nombre " + i);
+        //     i++;
+        // } while (i < x);
+
+        final int mytable[] = { 5, 9, 3, 4 };
+        for ( int a : mytable ) {
+         System.out.println("index:" +a);
+     }
+
+
+
+
     }
 }
