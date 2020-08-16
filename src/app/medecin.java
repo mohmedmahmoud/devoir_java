34ccc0280701a8c4fa5d42070d46e1c2a6fc9b25
mohmedmahmoud <bouhmed1996@gmail.com>
2020-08-16 14:54:33 +0200

    tous les exercice

--- src/app/medecin.java
+++ src/app/medecin.java
@@ -1,10 +1,9 @@
 package app;
 
-public class medecin extends personne{
-    medecin(String n,String p,String prof, int a){
+public class medecin extends personne {
+    medecin(String n, String p, String prof, int a) {
         super(n, p, prof, a);
-        
+
     }
-    
-    
+
 }
--- src/app/professeur.java
+++ src/app/professeur.java
@@ -1,8 +1,38 @@
 package app;
 
 public class professeur extends personne {
-    professeur(String n,String p,String prof, int a){
-        super(n, p, prof, a);
+    private String   etablissement;
+    professeur(String n,String p, int a){
+        super(n, p, a);
         
+    }
+    professeur(String n,String p,String prof, int a){
+        super(n, p,prof, a);
+        
+        
+    }
+
+    @Override
+    void sePresenter() {
+        
+       super.sePresenter();
+       System.out.println( " et je suis "+getProfission());
+    }
+    @Override
+    String getNom() {
+       
+        return super.getNom();
+    }
+    @Override
+    String getProfission() {
+     
+        return super.getProfission();
+    }
+    String getEtablissement(){
+        return etablissement;
+    }
+    void setEtablissement(String Etab){
+        etablissement=Etab;
+
     }
 }
